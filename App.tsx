2840075aab050107d97257bb5c9f400af3826364
rohxn-rai <rohan.rai932@outlook.com>
2025-01-03 07:44:54 -0500

    3rd commit - added a font family to the project for consistancy - configured font family to work seemlessly with all weights

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {getFontFamily} from './assets/fonts/helper';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text style={{fontSize: 50}}>Hello World!</Text>
+      <Text style={{fontSize: 50, fontFamily: getFontFamily('Inter', '900')}}>
+        Hello World!
+      </Text>
     </SafeAreaView>
   );
 };
